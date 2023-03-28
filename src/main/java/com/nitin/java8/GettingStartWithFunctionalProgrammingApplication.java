d4c3f720c6ca2ba802b793017836b12af04cda35
ngaikwad3 <ngaikwad3@dxc.com>
2023-03-28 14:55:20 +0530

    first commit

---
+++ src/main/java/com/nitin/java8/GettingStartWithFunctionalProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.nitin.java8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartWithFunctionalProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingStartWithFunctionalProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nitin/java8/GettingStartWithFunctionalProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitin.java8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingStartWithFunctionalProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
